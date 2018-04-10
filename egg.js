72e213a2124dd470104bf8cc75cdf3c56e381cde
Adam MacDonald <adam.macdonald-corrao@vistana.com>
2018-04-10 14:19:34 -0400

    Initial commit

---
+++ egg.js
@@ -0,0 +1,38 @@
+/*
+do(define(x, 10),
+   if(>(x, 5),
+      print("large"),
+      print("small")))
+*/
+
+function parseExpression(program) {
+	program = skipSpace(program);
+	let match, expr;
+	if (match = /^"([^"]*)"/.exec(program)) {
+		expr = {
+			type: 'value',
+			value: match[1]
+		};
+	} else if (match = /^\d+\b/.exec(program)) {
+		expr = {
+			type: 'value',
+			value: Number(match[0])
+		};
+	} else if (match = /^[^\s(),"]+/.exec(program)) {
+		expr = {
+			type: 'word',
+			name: match[0]
+		};
+	} else {
+		throw new SyntaxError('Unexpected syntax: ' + program);
+	}
+	return expr;
+}
+
+function skipSpace(string) {
+	let first = string.search(/\S/);
+	if (first == -1) {
+		return '';
+	}
+	return string.slice(first);
+}
